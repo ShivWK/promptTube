537a7710853d1a0b65b926b650c82690549567ec
Shivendra Dwivedi <shivendrawk@gmail.com>
2025-10-07 01:24:00 +0530

    Make side menu sliding

--- src/components/layout/header/SideMenu.jsx
+++ src/components/layout/header/SideMenu.jsx
@@ -1,10 +1,12 @@
-import { CircleUserRound, Home } from "lucide-react"
+import { CircleUserRound, Home, GripVertical } from "lucide-react"
+import { useState } from "react";
 
 const SideMenu = () => {
   const isSmall = window.innerWidth <= 786;
+  const [ showSideMenu, setShowSideMenu ] = useState(false);
 
   return (
-    <aside className="fixed top-1/2 left-0 -translate-y-1/2 rounded-e-2xl p-2 lg:p-2 flex flex-col dark:bg-gray-900">
+    <aside className={`fixed top-1/2 ${ showSideMenu ? "max-md:left-0" : "max-md:-left-19" } md:left-0 -translate-y-1/2 rounded-e-2xl p-2 lg:p-2 flex flex-col dark:bg-gray-900 transition-all duration-100 ease-linear`}>
       <div className="rounded-xl flex flex-col justify-center items-center py-1.5 px-2 lg:py-3 lg:px-4 active:bg-gray-400/30 hover:bg-gray-400/30 transition-all duration-100 ease-linear">
         <Home size={isSmall ? 25 : 40} className="dark:text-gray-200" />
         <span className="dark:text-gray-200 select-none">Home</span>
@@ -14,8 +16,12 @@ const SideMenu = () => {
         <CircleUserRound size={isSmall ? 25 : 40} className="dark:text-gray-200" />
         <span className="dark:text-gray-200 select-none">You</span>
       </div>
+
+      <div onClick={() => setShowSideMenu(!showSideMenu)} className="absolute top-1/2 -translate-y-1/2 -right-5 py-2 rounded-e-md bg-gray-900  dark:text-white md:hidden">
+        <GripVertical />
+      </div>
     </aside>
   )
 }
-
+{/* <GripVertical /> */}
 export default SideMenu;
