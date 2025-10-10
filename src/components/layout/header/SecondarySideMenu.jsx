b123219893d2616c727e8953540110d801c515a7
Shivendra Dwivedi <shivendrawk@gmail.com>
2025-10-10 19:05:51 +0530

    Add menu item

--- src/components/layout/header/SecondarySideMenu.jsx
+++ src/components/layout/header/SecondarySideMenu.jsx
@@ -1,5 +1,6 @@
-import { CircleUserRound, Home, GripVertical } from "lucide-react"
+import { CircleUserRound, Home, GripVertical, Brain } from "lucide-react"
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const SecondarySideMenu = () => {
   const isSmall = window.innerWidth <= 786;
@@ -8,15 +9,20 @@ const SecondarySideMenu = () => {
   return (
     <aside className={`fixed top-4/6 md:top-1/2 ${ showSideMenu ? "max-md:-left-1" : "max-md:-left-19" } md:-left-1 -translate-y-1/2 rounded-e-2xl p-2 flex flex-col dark:bg-gray-900 transition-all duration-100 ease-linear`}>
       
-      <div className="rounded-xl flex flex-col justify-center items-center py-1.5 px-2 lg:py-2 lg:px-3 active:bg-gray-400/30 hover:bg-gray-400/30 transition-all duration-100 ease-linear">
-        <Home size={isSmall ? 25 : 35} className="dark:text-gray-200" />
-        <span className="dark:text-gray-200 select-none">Home</span>
-      </div>
+      <Link to={"/"} className="rounded-xl flex flex-col justify-center items-center py-1.5 px-2 lg:py-2 lg:px-3 active:bg-gray-400/30 hover:bg-gray-400/30 transition-all duration-100 ease-linear">
+        <Home size={isSmall ? 25 : 30} className="dark:text-gray-200" />
+        <span className="dark:text-gray-200 text-sm select-none">Home</span>
+      </Link>
 
-      <div className="rounded-xl flex flex-col justify-center items-center py-1.5 px-2 lg:py-2 lg:px-3 active:bg-gray-400/30 hover:bg-gray-400/30 transition-all duration-100 ease-linear">
-        <CircleUserRound size={isSmall ? 25 : 35} className="dark:text-gray-200" />
-        <span className="dark:text-gray-200 select-none">You</span>
-      </div>
+      <Link to={"/gptBrowser"} className="rounded-xl flex flex-col justify-center items-center py-1.5 px-2 lg:py-2 lg:px-3 active:bg-gray-400/30 hover:bg-gray-400/30 transition-all duration-100 ease-linear">
+        <Brain size={isSmall ? 25 : 30} className="dark:text-gray-200" />
+        <span className="dark:text-gray-200 text-sm select-none">Promptly</span>
+      </Link>
+
+      <Link className="rounded-xl flex flex-col justify-center items-center py-1.5 px-2 lg:py-2 lg:px-3 active:bg-gray-400/30 hover:bg-gray-400/30 transition-all duration-100 ease-linear">
+        <CircleUserRound size={isSmall ? 25 : 30} className="dark:text-gray-200" />
+        <span className="dark:text-gray-200 text-sm select-none">You</span>
+      </Link>
 
       <div onClick={() => setShowSideMenu(!showSideMenu)} className="absolute top-1/2 -translate-y-1/2 -right-[1.4rem] py-2 rounded-e-md bg-gray-900  dark:text-white md:hidden">
         <GripVertical />
