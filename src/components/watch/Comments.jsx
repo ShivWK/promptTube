337e9188a4cbca4658984624088243fb0bb1713b
Shivendra Dwivedi <shivendrawk@gmail.com>
2025-11-02 02:21:46 +0530

    Show all comments by default in big screens

--- src/components/watch/Comments.jsx
+++ src/components/watch/Comments.jsx
@@ -1,6 +1,6 @@
 import { useLazyGetCommentsQuery } from "../../features/watch/watchApiSlice";
 import { selectIsSmall } from "../../features/home/homeSlice";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { CircleUserRound } from "lucide-react";
 import { useSelector } from "react-redux";
 import Comment from "./CommentCard"
@@ -19,9 +19,21 @@ const Comments = ({ id }) => {
         setShowCompleteComment(false)
     }
 
+    useEffect(() => {
+        if (!isSmall) {
+            setShowCompleteComment(true)
+        }
+    }, [isSmall])
+
+    const commentBoxClickHandler = () => {
+        if (isSmall) {
+            setShowCompleteComment(true)
+        }
+    }
+
     return (
         isLoading ? <p>Loading...</p>
-            : <div onClick={() => setShowCompleteComment(true)} className={`relative w-full rounded-md transition-all duration-150 ease-linear dark:bg-gray-800 p-2 pb-1 dark:text-gray-200 mt-1 pretty-scrollbar ${showCompleteComment ? "overflow-auto" : "overflow-hidden cursor-pointer"}`}>
+            : <div onClick={commentBoxClickHandler} className={`relative w-full rounded-md transition-all duration-150 ease-linear dark:bg-gray-800 p-2 pb-1 dark:text-gray-200 mt-1 pretty-scrollbar ${showCompleteComment ? "overflow-auto" : "overflow-hidden cursor-pointer"}`}>
 
                 {showCompleteComment && <div className="flex items-center gap-2 md:gap-3 mb-2 bg-gray-800 w-full">
                     <CircleUserRound size={isSmall ? 55 : 55} strokeWidth={1} className="dark:text-primary" />
