c3284c5effff5a88b4171e3da5082b4074bf83bd
Shivendra Dwivedi <shivendrawk@gmail.com>
2026-07-07 02:26:54 +0530

    fix: Subscription loading bug and hight

--- src/components/account/Account.jsx
+++ src/components/account/Account.jsx
@@ -9,37 +9,38 @@ const Account = () => {
   const section = searchParams.get("section");
 
   const scrollToRequiredSection = (section) => {
-    document.getElementById(section).scrollIntoView({
+    document.getElementById(section)?.scrollIntoView({
       behavior: "smooth",
-      block: "start"
-    })
-  }
+      block: "start",
+    });
+  };
 
   useEffect(() => {
     if (section === "history") {
-      scrollToRequiredSection("history")
+      scrollToRequiredSection("history");
     } else if (section === "watchLater") {
-      scrollToRequiredSection("watchLater")
+      scrollToRequiredSection("watchLater");
     } else if (section === "likedVideos") {
-      scrollToRequiredSection("likedVideos")
-    } else if (section === 'subscription') {
-      scrollToRequiredSection("subscriptions")
+      scrollToRequiredSection("likedVideos");
+    } else if (section === "subscription") {
+      scrollToRequiredSection("subscriptions");
     }
-  }, [section])
+  }, [section]);
 
   return (
-    <main className="pt-20 lg:pt-20 pb-12 md:pb-2 min-h-[100vh] md:h-[100vh]">
-      <section className="max-w-[1210px] md:rounded-2xl mx-auto flex flex-col md:flex-row max-md:justify-center gap-4 md:gap-1 h-full">
-        <div className="basis-full h-full md:basis-[22%] min-w-0 md:bg-primary/40 mx-auto md:rounded-s-2xl w-full">
+    <main className="pt-20 pb-12 md:pb-2 min-h-screen">
+      <section className="mx-auto md:min-h-[calc(100vh-6rem)] flex max-w-[1210px] flex-col gap-4 md:flex-row md:items-stretch md:gap-1 md:rounded-2xl">
+        <div className="basis-full md:basis-[22%] min-w-0 w-full md:rounded-s-2xl md:bg-primary/40">
           <ProfileCard />
           <Subscriptions />
         </div>
+
         <div className="basis-full md:basis-[77%] min-w-0">
           <SavedVideosSection />
         </div>
       </section>
     </main>
-  )
-}
+  );
+};
 
 export default Account;
--- src/components/common/ChannelCard.jsx
+++ src/components/common/ChannelCard.jsx
@@ -5,7 +5,6 @@ import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 
 const ChannelCard = ({ object }) => {
-    // console.log(object)
     const navigate = useNavigate();
     const dispatch = useDispatch();
 
@@ -21,7 +20,7 @@ const ChannelCard = ({ object }) => {
         <div onClick={cardClickHandler} className="flex shrink-0 items-center gap-2 md:gap-3 dark:text-gray-100 rounded-lg bg-gray-900 md:pr-3 p-2 cursor-pointer hover:bg-gray-700 active:bg-gray-700 transition=all duration-150 ease-linear">
            <img src={object?.snippet?.thumbnails?.default?.url} alt="channel_logo" className="rounded-full h-11 md:h-13 w-11 md:w-13 border border-gray-400" />
             <div className="h-fit flex flex-col md:gap-1 md:pr-2">
-                <h2 className="text-sm md:text-lg leading-5 tracking-wider max-w-38 md:max-w-[11rem] truncate">{object?.snippet?.title}</h2>
+                <h2 className="text-sm leading-5 tracking-wide max-w-38 md:max-w-[8.5rem] truncate">{object?.snippet?.title}</h2>
                 <p className="text-sm dark:text-gray-300">{countViews(object?.statistics?.subscriberCount)}</p>
             </div>
         </div>
