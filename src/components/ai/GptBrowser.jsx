bb52603d3a0d97ad840648e7949ee2a22a9a5e9a
Shivendra Dwivedi <shivendrawk@gmail.com>
2026-08-10 13:34:24 +0530

    Fix: ecliped path bug

--- src/components/ai/GptBrowser.jsx
+++ src/components/ai/GptBrowser.jsx
@@ -9,23 +9,34 @@ const GptBrowser = () => {
 
     return (
         <>
-            <header className='absolute animate-slideDown left-0 w-full h-30 lg:h-45 bg-gradient-to-tr from-primary via-blue-500 to-green-400 p-6 flex flex-col items-center' style={{
-                clipPath: "ellipse(71% 59% at 50% 40%)"
-            }}>
-                <div className="self-start justify-self-start">
-                    <LogoAndMenu showMenu={false} />
-                </div>
+            <header className="absolute left-0 w-full h-30 lg:h-45">
+                {/* Background shape */}
+                <div
+                    className="absolute inset-0 animate-slideDown bg-gradient-to-tr from-primary via-blue-500 to-green-400 pointer-events-none"
+                    style={{
+                        clipPath: "ellipse(71% 59% at 50% 40%)"
+                    }}
+                />
 
-                <div className="flex gap-2 items-center justify-self-center text-white text-2xl lg:text-4xl font-bold tracking-wide mt-1 lg:mt-10">
-                    <Sparkles
-                        size={isSmall ? 20 : 30} strokeWidth={1.5}
-                        className="text-primary"
-                    />
-                    <span className="">Promptly</span>
-                    <Sparkles
-                        size={isSmall ? 20 : 30} strokeWidth={1.5}
-                        className="text-primary"
-                    />
+                {/* Actual interactive content */}
+                <div className="relative z-50">
+                    <div className="cursor-pointer self-start mt-2.5 md:mt-4 ml-2.5 md:ml-4">
+                        <LogoAndMenu showMenu={false} />
+                    </div>
+
+                    <div className="flex gap-2 items-center justify-self-center text-white text-2xl lg:text-4xl font-bold tracking-wide mt-4 lg:mt-12">
+                        <Sparkles
+                            size={isSmall ? 20 : 30}
+                            strokeWidth={1.5}
+                            className="text-primary"
+                        />
+                        <span className="select-none">Promptly</span>
+                        <Sparkles
+                            size={isSmall ? 20 : 30}
+                            strokeWidth={1.5}
+                            className="text-primary"
+                        />
+                    </div>
                 </div>
             </header>
             <main className={"animate-smoothShow pt-38 lg:pt-54 flex flex-col justify-center gap-8"}>
--- src/components/layout/header/LogoAndMenu.jsx
+++ src/components/layout/header/LogoAndMenu.jsx
@@ -1,10 +1,9 @@
 import { Menu } from "lucide-react";
-import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { setSidebar } from "../../../features/home/homeSlice";
+import { Link } from "react-router-dom";
 
 const LogoAndMenu = ({ showMenu = true, showName = false }) => {
-    const navigate = useNavigate();
     const dispatch = useDispatch();
 
     const menuClickHandler = () => {
@@ -14,11 +13,6 @@ const LogoAndMenu = ({ showMenu = true, showName = false }) => {
             mode: 'All',
             value: true,
         }))
-    }
-
-    const logoClickHandler = () => {
-        if (!showMenu) return;
-        navigate("/")
     }
 
     return (
@@ -31,13 +25,13 @@ const LogoAndMenu = ({ showMenu = true, showName = false }) => {
                     />
                 </button>
             }
-            <div onClick={logoClickHandler} className={`flex gap-3 items-center ${showMenu && "cursor-pointer"}`}>
-                <img src="/favicon/android-chrome-192x192.png" alt="site_logo" className={`${showMenu ? "h-10 lg:h-10 w-11 lg:w-11" : "h-11 lg:h-12 w-12 lg:w-13"}`}></img>
+            <Link to="/" className="flex gap-3 items-center cursor-pointer">
+                <img src="/favicon/android-chrome-192x192.png" alt="site_logo" className={`${showMenu ? "h-10 lg:h-10 w-11 lg:w-11" : "h-11 lg:h-12 w-12 lg:w-13"}`} />
                 <h1 className={`text-primary font-bold text-3xl ${!showName && "hidden md:inline"} select-none`}>
                     <span className="text-gray-100">Prompt</span>
                     <span>Tube</span>
                 </h1>
-            </div>
+            </Link>
         </div>
     )
 }
