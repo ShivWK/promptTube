7503a0ca055fcfbe4b5904deea945f4433d81944
Shivendra Dwivedi <shivendrawk@gmail.com>
2026-08-28 14:18:24 +0530

    update some style

--- src/components/auth/PasswordReset.jsx
+++ src/components/auth/PasswordReset.jsx
@@ -5,7 +5,15 @@ const PasswordReset = ({ resetPasswordLoading, resetPasswordMailSend }) => {
         <div className="flex flex-col gap-2">
             {resetPasswordMailSend && <p className="text-center text-green-400 leading-6 tracking-wide font-medium lg:text-lg">Email sent successfully! Check your inbox or spam folder if you don’t see it.</p>}
 
-            <button type="submit" className={`flex items-center justify-center w-full ${resetPasswordMailSend ? "bg-gray-500" : "bg-primary"} text-white font-semibold tracking-wide py-1.5 lg:py-2 rounded lg:text-xl ${(!resetPasswordLoading && !resetPasswordMailSend) && "active:scale-95 hover:bg-[#df0421]"} transform transition-all duration-75 ease-linear cursor-pointer mt-2`}>
+            <button
+                type="submit"
+                className={`flex items-center justify-center w-full text-white font-medium tracking-wide rounded lg:text-lg  
+                ${resetPasswordLoading ? "py-2" : "py-1.5"}
+                ${resetPasswordMailSend ? "bg-gray-500" : "bg-primary"} 
+                ${(!resetPasswordLoading && !resetPasswordMailSend)
+                    && "active:scale-95 hover:bg-[#df0421]"} transform transition-all duration-75 ease-linear cursor-pointer mt-2`
+                }
+            >
                 {resetPasswordLoading
                     ? <DotBounceLoader />
                     : resetPasswordMailSend
